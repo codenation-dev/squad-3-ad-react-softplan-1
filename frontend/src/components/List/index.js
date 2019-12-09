1e36dbd4bbc6a49860bbae36695261b9980cd98c
Cleverson Pohlod <cleverson@softplan.com.br>
2019-12-09 11:37:06 -0300

    feat: criação do componente de perfil,alteração da route para minusculo, ajuste na chamada da lista para o perfil

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { IconButton,  Menu } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 
 import Header from '../Header';
 import GridView from '../GridView';
+import Perfil from '../Perfil';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,21 +20,14 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function List() {
+  
+  const [elRef, setElRef] = useState(null);
+
   const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const auth = true;
 
-  const handleChange = event => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handlePerfil = event => {
+    setElRef(elRef ? null : event.currentTarget);
   };
 
   return (
@@ -45,13 +39,14 @@ export default function List() {
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
-                onClick={handleMenu}
+                onClick={handlePerfil}
                 color="inherit"
               >
-                <AccountCircle />
+              <AccountCircle />
               </IconButton>
             </div>
           )}
+          <Perfil elRef={elRef}> </Perfil>
       </Header>
       <GridView />
     </div>
