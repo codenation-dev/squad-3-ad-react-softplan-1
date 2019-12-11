64c619ff57f6d74b55c223f740111f020da85e11
R1tter <marcelosori@hotmail.com>
2019-12-10 22:14:26 -0300

    add login redux/ dependencias relacionadas

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
-// import { Container } from './styles';
+import { signInRequest } from "../../store/modules/auth/actions";
+// Sugestão para o signUp, copiar esse esquema e apenas adicionar o nome e a linha que vou deixar.
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email(`Insira um e-mail válido`)
+    .required(`O e-mail é obrigatório`),
+  password: Yup.string().required(`A senha é obrigatória`)
+  // no singup pode por .min(6, 'a senha deve conter no minimo seis dígitos.')
+});
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <>
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha Secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
