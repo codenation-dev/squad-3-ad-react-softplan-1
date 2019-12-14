1fce08a6d5d8c915e3c63d1a58309faeb6261385
Marcelo Ritter <marceloritter@MacBook-Pro-de-Marcelo.local>
2019-12-14 17:29:00 -0300

    dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,21 +1,53 @@
 import React from "react";
-import GridView from "../../components/GridView";
-import Button from "../../components/Button";
+import api from '../../services/api';
+import { Input } from '@rocketseat/unform';
 
+import { Container, Log } from './styles'
 
 const Dashboard = (props) => {
+   return <Container>
+      <header>
+         <select name="level" >
+            <option value="value0" selected>Selecionar</option>
+            <option value="value1">Produção</option>
+            <option value="value2">Homologação</option>
+            <option value="value3">Dev</option>
+         </select>
+         <select name="type" >
+            <option value="value0" selected>Ordenar por</option>
+            <option value="value1">Level</option>
+            <option value="value2">Frequencia</option>
+         </select>
+         <select name="type" >
+            <option value="value0" selected>Buscar por</option>
+            <option value="value1">Level</option>
+            <option value="value2">Descrição</option>
+            <option value="value3">Origem</option>
+         </select>
+         <Input name="name" placeholder="Pesquisar aqui"></Input>
+      </header>
+      
 
-  const handleSair = () => {
-    localStorage.removeItem("central-erros-auth-token");
-    props.history.push("/");
-  }
-
-  return (
-    <div className="dashboard">
-      <Button label={'Sair'}  onClick={handleSair} />
-      <GridView />
-    </div>
-  )
+         <ul>
+            <Log>
+               <strong>Data teste</strong>
+               <span>Teste Nome</span>
+            </Log>
+            <Log>
+               <strong>Data teste</strong>
+               <span>Teste Nome</span>
+               <span>Qts</span>
+            </Log>
+            <Log>
+               <strong>Data teste</strong>
+               <span>Teste Nome</span>
+            </Log>
+            <Log>
+               <strong>Data teste</strong>
+               <span>Teste Nome</span>
+            </Log>
+         </ul>
+   </Container>
 }
 
 export default Dashboard;
