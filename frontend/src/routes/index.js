4ea91aaddc572c35e4879bba8e9cdba8b1ff5238
Jerson Seling <jersonseling@yahoo.com.br>
2019-12-13 22:30:56 -0200

    detalhe

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -7,6 +7,7 @@ import SignUp from "../pages/SignUp";
 import Profile from "../pages/Profile";
 
 import Dashboard from "../pages/Dashboard";
+import Detail from "../pages/Detail";
 
 export default function Routes() {
   return (
@@ -16,6 +17,7 @@ export default function Routes() {
         <Route path="/register" component={SignUp} />
         <Route path="/dashboard" component={Dashboard} isPrivate />
         <Route path="/profile" component={Profile} isPrivate />
+        <Route path="/detail/:id_log" component={Detail} isPrivate />
 
         <Route path="*" component={<h1>404 Page Not Found</h1>} />
       </Switch>
