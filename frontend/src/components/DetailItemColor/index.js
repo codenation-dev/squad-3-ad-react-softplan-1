48d73fc50c9906f55c23cc053172423ad6e5c725
Jerson dos Santos Seling <jerson.seling@softplan.com.br>
2019-12-15 15:21:14 -0300

    suporte a mais tipos

--- frontend/src/components/DetailItemColor/index.js
+++ frontend/src/components/DetailItemColor/index.js
@@ -5,13 +5,16 @@ const DetailItemText = (props) => {
     let styledColor = "";
 
     if (props.type === "WARNING")
-        styledColor = "is-danger";
+        styledColor = "is-warning";
 
     if (props.type === "INFO")
         styledColor = "is-info";
 
     if (props.type === "DEBUG")
-        styledColor = "is-warning";      
+        styledColor = "is-primary";   
+        
+    if (props.type === "ERROR")
+        styledColor = "is-danger";           
 
     if (styledColor === "")
         return <></>
