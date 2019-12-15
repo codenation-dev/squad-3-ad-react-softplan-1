97577d6e427da4413500df38de360bb4d25f9f1a
Jaquiel Paim <jaquiel.paim@gmail.com>
2019-12-15 13:37:35 -0300

    Filtro Level

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { Input } from '@rocketseat/unform';
 import { Link } from "react-router-dom";
@@ -6,49 +6,130 @@ import { Link } from "react-router-dom";
 import { Container, Log } from './styles'
 
 const Dashboard = (props) => {
-   return <Container>
-      <header>
-         <select name="level" >
-            <option value="value0" selected>Selecionar</option>
-            <option value="value1">Produção</option>
-            <option value="value2">Homologação</option>
-            <option value="value3">Dev</option>
+   const [logs, setLogs] = useState([]);
+   const [dataSource, setDataSource] = useState([]);
+
+   useEffect(() => {
+     const token = localStorage.getItem("central-erros-auth-token");
+     console.log(token);
+    getLogs(token);
+   },[]);
+ 
+   const getLogs = ( token) => {
+      fetch(`https://centralerrosapp.herokuapp.com/logs/${token}`)
+      .then(function(response){
+        return response.text();
+      }).then(data => {
+          console.log(data);
+          setLogs(JSON.parse(data));
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+    };
+
+    const handleChangeLevel = e => {
+        const token = localStorage.getItem("central-erros-auth-token");
+        
+        setLogs(getLogs(token));
+
+        if (e.target.value !== 'default') {
+        setLogs(
+            logs.filter(log => 
+                log.type === e.target.value
+            )
+        );
+      }
+
+        console.log(e.target.value);
+        
+        console.log(e.target.name);
+    };
+
+
+    const handleChange = e => {}
+
+    const handleArquivar = () => {
+
+    }
+
+    const handleDeletar = () => {
+       
+    }
+
+    const handleAnterior = () => {
+
+    }
+
+    const handleProxima = () => {
+       
+    }
+
+   return (
+   <Container>
+         <header>
+         <select name="level" onChange={handleChangeLevel}>
+            <option value="default" selected>Filtrar por Level</option>
+            <option value="DEBUG">DEBUG</option>
+            <option value="ERROR">ERROR</option>
+            <option value="INFO">INFO</option>
+            <option value="WARNING">WARNING</option>
          </select>
-         <select name="type" >
-            <option value="value0" selected>Ordenar por</option>
-            <option value="value1">Level</option>
-            <option value="value2">Frequencia</option>
+         <select name="type" onChange={handleChange}>
+            <option value="default" selected>Ordenar por</option>
+            <option value="level">Level</option>
+            <option value="quantity">Frequencia</option>
          </select>
-         <select name="type" >
-            <option value="value0" selected>Buscar por</option>
-            <option value="value1">Level</option>
-            <option value="value2">Descrição</option>
-            <option value="value3">Origem</option>
+         <select name="type" onChange={handleChange}>
+            <option value="default" selected>Buscar por</option>
+            <option value="level">Level</option>
+            <option value="name">Descrição</option>
+            <option value="origin">Origem</option>
          </select>
          <Input name="name" placeholder="Pesquisar aqui"></Input>
       </header>
-      
-
          <ul>
+         {        
+            logs === [] ?
+            (
             <Log>
-               <strong>Data teste</strong>
-               <span>Teste Nome</span>
-            </Log>
-            <Log>
-               <strong>Data teste</strong>
-               <span>Teste Nome</span>
-               <span>Qts</span>
-            </Log>
-            <Log>
-               <Link to="/">Data teste</Link>
-               <span>Teste Nome</span>
-            </Log>
-            <Log>
-               <strong>Data teste</strong>
-               <span>Teste Nome</span>
-            </Log>
+            <strong>sem dados</strong>
+            </Log>              
+            ) :
+            (
+               logs.map( (log, idx) => {
+                  return (
+                  <>
+                  <Log key={idx}>
+                  <strong>Level</strong>
+                  <span>{log.type}</span>
+                  <button type="button" onClick={handleArquivar}>Arquivar</button>
+                  <button type="button" onClick={handleDeletar}>Deletar</button>
+                  </Log>         
+                  <Log key={idx}>
+                  <strong>Log</strong>
+                  <span>{log.name}</span>
+                  <span>{log.origin}</span>
+                  <span>{log.data}</span>
+                  <button type="button" onClick={handleArquivar}>Arquivar</button>
+                  <button type="button" onClick={handleDeletar}>Deletar</button>
+                  </Log>         
+                  <Log key={idx}>
+                  <strong>Eventos</strong>
+                  <span>{log.quantity}</span>
+                  <button type="button" onClick={handleArquivar}>Arquivar</button>
+                  <button type="button" onClick={handleDeletar}>Deletar</button>
+                  </Log>        
+                  </> 
+                  );
+               })       
+            )
+         }
          </ul>
+         <button type="button" onClick={handleAnterior}>Anterior</button>
+         <button type="button" onClick={handleProxima}>Próxima</button>
    </Container>
+   )
 }
 
 export default Dashboard;
