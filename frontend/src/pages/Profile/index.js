f65f50061e4db2130b657d8b7f0618cd095fa939
cpohlod <cleverson.pohlod@gmail.com>
2019-12-15 04:40:04 -0300

    implementado para salvar o perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,20 +1,35 @@
 import React , { useEffect, useState } from 'react';
 import { Container } from './styles'
 import { Form, Input } from '@rocketseat/unform';
-import { getUser } from '../../services/api.js'
-import axios from "axios";
+import { register } from '../../services/api.js'
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required(`O nome é obrigatório.`),
+  email: Yup.string()
+    .required(`O nome é obrigatório.`),
+
+  pwd: Yup.string()
+               .required(`A senha é obrigatória.`)
+               .min(6, 'A senha deve conter no mínimo seis dígitos.')
+});
 
 export default function Profile(props) {
   const [nome, setNome] = useState();
   const [email, setEmail] = useState();
-  const API = axios.create({ baseURL: "https://centralerrosapp.herokuapp.com" });
-  
+
   useEffect(() => {
     const token = localStorage.getItem("central-erros-auth-token");
     console.log(token);
     getUser(token);
   },[]);
   
+  const handleSubmit = ({name, email, pwd}) => {
+    console.log("salva perfil");
+    register(props.history, name, email, pwd)
+  }
+
   const handleSair = () => {
     localStorage.removeItem("central-erros-auth-token");
     props.history.push("/");
@@ -34,14 +49,14 @@ export default function Profile(props) {
   };
   
   return  <Container>
-    <Form>
+    <Form onSubmit={handleSubmit} schema={schema}>
       <Input name="name" placeholder="Nome completo" value={nome}/>
       <Input name="email" type="email" placeholder="Seu endereço de email"  value={email}/>
 
       <hr/>
 
       <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
-      <Input type="password" name="Password" placeholder="Nova senha" />
+      <Input type="password" name="pwd" placeholder="Nova senha" />
       <Input type="password" name="ConfirmPassword" placeholder="Confirmação de senha" />
 
       <button type="submit">Atualizar perfil</button>
