be787d058a3614edb2e86832511cddbfe433873c
cpohlod <cleverson.pohlod@gmail.com>
2019-12-15 04:20:11 -0300

    mostrar dados do perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,26 +2,41 @@ import React , { useEffect, useState } from 'react';
 import { Container } from './styles'
 import { Form, Input } from '@rocketseat/unform';
 import { getUser } from '../../services/api.js'
+import axios from "axios";
 
 export default function Profile(props) {
-  const [login, setLogin] = useState({});
-
+  const [nome, setNome] = useState();
+  const [email, setEmail] = useState();
+  const API = axios.create({ baseURL: "https://centralerrosapp.herokuapp.com" });
+  
   useEffect(() => {
     const token = localStorage.getItem("central-erros-auth-token");
     console.log(token);
-    getUser(token, setLogin);
-    console.log(login.name);
+    getUser(token);
   },[]);
   
   const handleSair = () => {
     localStorage.removeItem("central-erros-auth-token");
     props.history.push("/");
   }
-
-  return <Container>
+  const getUser = ( token) => {
+    fetch(`https://centralerrosapp.herokuapp.com/findlogin/${token}`)
+    .then(function(response){
+      return response.text();
+    }).then(data => {
+        console.log(data);
+        setNome(JSON.parse(data).name);
+        setEmail(JSON.parse(data).email);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+  
+  return  <Container>
     <Form>
-      <Input name="name" placeholder="Nome completo" />
-      <Input name="email" type="email" placeholder="Seu endereço de email"/>
+      <Input name="name" placeholder="Nome completo" value={nome}/>
+      <Input name="email" type="email" placeholder="Seu endereço de email"  value={email}/>
 
       <hr/>
 
