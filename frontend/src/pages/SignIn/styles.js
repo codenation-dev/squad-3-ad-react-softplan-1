5df57dacae2e8deb2f44485ee479db7f6da48988
Cleverson Pohlod <cleverson@softplan.com.br>
2019-12-16 11:31:41 -0300

    implementada a validação do usuário e senha no login

---
+++ frontend/src/pages/SignIn/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Mensagem = styled.div`
+
+label {
+  text-align: center;
+  margin: 0 auto;
+  color: red;
+}
+
+div {
+  display: flex;
+  justify-content: center;     
+  flex-wrap: wrap; 
+}
+`
+
